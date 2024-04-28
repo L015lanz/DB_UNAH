e7148768fc6e786697961ee81db778ee9f9edd72
l015 <luisglainezw001@gmail.com>
2024-04-28 15:43:43 -0600

    integracion del lector de contenidos

--- DB_Script/urls.py
+++ DB_Script/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+app_name='DB_UNAH'
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('sequence',views.sequence, name="sequence")
 ]
--- DB_Script/views.py
+++ DB_Script/views.py
@@ -1,6 +1,25 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
+from django.contrib.staticfiles import finders
+import os
+from DB_UNAH.settings import STATIC_ROOT
 
-# Create your views here.
+def sequence(request):
+    print(STATIC_ROOT)
+
+    directorio_textos = os.path.join(STATIC_ROOT, 'script')
+    ruta_archivo = os.path.join(directorio_textos, 'sequence.txt')
+    with open(ruta_archivo, 'r') as archivo:
+        contenido = archivo.read()
+
+    if ruta_archivo:
+        with open(ruta_archivo, 'r') as archivo:
+            contenido = archivo.read()
+
+        return render(request, 'sequence.html', {'contenido': contenido})
+    else:
+        return HTTPResponse("El archivo no fue encontrado.")
+
 def home(request):
     return render(request,"index.html")
 
